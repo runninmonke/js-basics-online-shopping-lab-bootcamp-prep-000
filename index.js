4c157a6586bc3bc61afe0a8bdbf87246360484fa
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-09-08 19:08:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -38,7 +38,11 @@ function viewCart() {
 }
 
 function total() {
-  // write your code here
+  var totalCost = 0
+  for (var i = 0; i < cart.length; i++) {
+    totalCost += cart[i][Object.keys(cart[i])[0]]
+  }
+  return totalCost
 }
 
 function removeFromCart(item) {
